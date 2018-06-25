9303521878c3f4cda2bb7f935119cec963a88f43
John Bateman <jbateman415@gmail.com>
2018-06-25 18:36:50 +0000

    added anonymous jquery function to scripts.js and a button to index

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
+/* global $ */
+// the previous comment makes global warning go away
+
 $( document ).ready( function () {
 
-  console.log("Hello world!");
+  console.log("Document was loaded");
 
 });
+
+// The following uses jquery to select elements with jquery selector
+var myButton = $("button") //$ triggers call to jquery selector to select button element, and return it to a var
+myButton.click( function() {
+    console.log("myButton was pressed!");
+} );  //an anonymous function
